f5a4dfc2ff5564603f17061ec2c7e6400e02e90d
Ian Dela Cruz <iankencruz@gmail.com>
2023-08-01 17:53:00 +1000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
-
-import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), react()]
+  integrations: [image(), svelte()]
 });
